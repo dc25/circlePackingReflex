07e2d192e63f0e009e058f9b94d159b7e43918d8
Dave Compton <davecompton7@gmail.com>
2018-04-24 20:01:11 +0000

    updated for current reflex.

--- Main.hs
+++ Main.hs
@@ -1,46 +1,56 @@
 {-# Language RecursiveDo #-}
+{-# LANGUAGE OverloadedStrings #-}
+import Data.Text (Text, pack, unpack)
 import Optimisation.CirclePacking
 import Reflex.Dom
-import Data.Map (fromList)
+import Data.Map (Map, fromList)
+
+packShow :: Show a => a -> Text
+packShow = (pack.show)
 
 width  = 400
 height = 400
 
+colors :: [String]
 colors = ["Green","Silver", "Lime", "Gray", "Olive", "Yellow", "Maroon", "Navy", "Red", "Blue", "Purple", "Teal", "Fuchsia", "Aqua"]
 
 svgNamespace = Just "http://www.w3.org/2000/svg"
 
 showCircle :: MonadWidget t m => ((String,Double), (Double,Double)) -> m ()
 showCircle ((color, radius), (x, y)) = do
-    let circleAttrs = fromList [ ( "cx", show x)
-                               , ( "cy", show y)
-                               , ( "r",  show radius)
-                               , ( "style",  "fill:" ++ color) ] 
+    let circleAttrs = fromList [ ( "cx", packShow x)
+                               , ( "cy", packShow y)
+                               , ( "r",  packShow radius)
+                               , ( "style",  pack $ "fill:" ++ color) ] 
 
     elDynAttrNS' svgNamespace "circle" (constDyn circleAttrs) $ return ()
 
     return ()
 
 
-svgAttrs = fromList [ ( "viewBox" ,            show (-width / 2) 
+svgAttrs :: Map Text Text
+svgAttrs = fromList [ ( "viewBox" , pack (
+                                        show (-width / 2) 
                                      ++ " " ++ show (-height / 2) 
                                      ++ " " ++ show width 
-                                     ++ " " ++ show height)
-                    , ( "width" ,    show width)
-                    , ( "height" ,   show height) 
+                                     ++ " " ++ show height) )
+                    , ( "width" ,    packShow width)
+                    , ( "height" ,   packShow height) 
                     ] 
 
+stringToCircle :: MonadWidget t m => Text -> m [()]
 stringToCircle =    mapM showCircle
                   . packCircles snd
                   . zip (cycle colors)
                   . map read
                   . words
+                  . unpack
 
 main :: IO()
 main = mainWidget $ do
     rec
         let dString = value ti
-        dCircles <- mapDyn stringToCircle dString
+            dCircles = fmap stringToCircle dString
         elDynAttrNS' svgNamespace "svg" (constDyn svgAttrs) $ dyn dCircles
         el "br" $ return ()
         ti <- textInput $ def { _textInputConfig_initialValue 
